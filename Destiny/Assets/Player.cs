d8ccec980e86af95f35c13ac442154e9d9280cf1
abhi <vadabhishek@gmail.com>
2020-11-17 21:42:02 +0530

    added frictionless material

---
+++ Destiny/Assets/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10f;
+    public float force = 10f;
+    private Rigidbody rigid;
+
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        rigid.AddRelativeForce(new Vector3(-speed, 0, 0));
+
+    }
+    private void Update()
+    {
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigid.AddRelativeForce(new Vector3(0,0,-force));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rigid.AddRelativeForce(new Vector3(0, 0, force));
+        }
+    }
+}
